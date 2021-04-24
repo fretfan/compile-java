66f32b897d840d8e32718bcfd6d6a43dd0269914
Sergei Kossik <fretfan92@gmail.com>
2021-04-24 16:12:23 +0300

    compile classes + strin-utils jar

---
+++ Asd.java
@@ -0,0 +1,13 @@
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Asd {
+    public static void main(String[] args1 ){
+        Qwe.print();
+
+        var msg = "some-text";
+        msg = StringUtils.capitalize(msg);
+
+        System.out.println("Executed Asd.main");
+    }
+}
---
+++ Qwe.java
@@ -0,0 +1,5 @@
+public class Qwe {
+    public static void print() {
+        System.out.println("Qwe.print");
+    }
+}
